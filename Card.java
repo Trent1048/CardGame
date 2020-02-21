ff399afe652f3c09866b048f40c4b6ba25781f4c
unknown <JPOWERS@STARFLEET.LOCAL>
2020-02-21 14:20:32 -0800

    Fixed some brackets and fixed toString

--- Card.java
+++ Card.java
@@ -13,16 +13,16 @@ public class Card {
 
     public int value;
 
-    public Card(Suit suit, int value)
-    {
-        if(value >= 1 && value <= 13)
-        {
+    public Card(Suit suit, int value) {
+        if(value >= 1 && value <= 13) {
             this.suit = suit;
             this.value = value;
         }
     }
-    public String toString()
-    {
-        return (suits[this.value-1]+" of "+this.suit.toString().toLowerCase());
+    public String toString() {
+        String raw = this.suit.toString();
+        String firstChar = raw.substring(0, 1);
+        String theRest = raw.substring(1, raw.length()-1).toLowerCase();
+        return (suits[this.value-1]+" of "+firstChar+theRest+"s");
     }
 }

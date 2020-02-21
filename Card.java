3cbdcea97c175fc54c16c8be95aa4eaeacae7f55
Trent1048 <trentbultsma@gmail.com>
2020-02-21 14:51:10 -0800

    add turn

--- Card.java
+++ Card.java
@@ -9,9 +9,17 @@ public class Card {
 
     public static String[] ranks = new String[] {"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"};
 
-    public Suit suit;
+    private Suit suit;
 
-    public int value;
+    private int value;
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
 
     public Card(Suit suit, int value) {
         if(value >= 1 && value <= 13) {
--- Game.java
+++ Game.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Game {
 
+    // deck is a stack because we take cards off the top like in real life
     public static Stack<Card> deck;
     private static ArrayList<Card> playerHand;
     private static ArrayList<Card> botHand;
@@ -31,5 +32,19 @@ public class Game {
             playerHand.add(deck.pop());
             botHand.add(deck.pop());
         }
+    }
+
+    // checks if a card with the number cardNum is in 
+    // the otherHand and moves it over to hand if it is
+    // returns true if the card was found and moved, false if not
+    private static boolean turn(ArrayList<Card> hand, ArrayList<Card> otherHand, int cardNum) {
+        for(Card card : otherHand) {
+            if(card.getValue() == cardNum) {
+                otherHand.remove(card);
+                hand.add(card);
+                return true;
+            }
+        }
+        return false;
     }
 }
